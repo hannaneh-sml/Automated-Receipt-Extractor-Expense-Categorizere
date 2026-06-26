9eea0bf4f2b39d5855d868cf07834b01592b2b35
Hannaneh <hannaneh.safarmmdloo@gmail.com>
2026-06-26 08:54:49 +0330

    feat: create redis container, push user image+prompt to redis in queue-publisher.py

--- gateway_service/src/services/queue_publisher.py
+++ gateway_service/src/services/queue_publisher.py
@@ -1,10 +1,21 @@
+import redis
+import json
+import base64
+
+redis_client = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+
 def publish_job(job_id: str, image_bytes: bytes, user_prompt: str, filename: str):
+    image_b64 = base64.b64encode(image_bytes).decode('utf-8')
     
-    print("="*50)
-    print(f"✅ NEW JOB CREATED: {job_id}")
-    print(f"📁 File: {filename} ({len(image_bytes)} bytes)")
-    print(f"📝 User Prompt: '{user_prompt}'")
-    print("🚀 Pushing to OCR Worker Queue...")
-    print("="*50)
-    #TBD
+    payload = {
+        "job_id": job_id,
+        "filename": filename,
+        "user_prompt": user_prompt,
+        "image_data": image_b64,
+        "status": "QUEUED"
+    }
+    
+    redis_client.lpush("ocr_task_queue", json.dumps(payload))
+    
+    print(f"✅ Successfully published Job {job_id} to Redis queue 'ocr_task_queue'")
     return True
